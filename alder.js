b38ef4f7051c7ab8c48ff28a454d77d908dd20df
Christian Hasselberger <chasselberger@stripe.com>
2017-06-26 18:45:06 -0700

    made js faster

--- alder.js
+++ alder.js
@@ -1,18 +1,8 @@
-// $(document).ready(function() {
-//   $(".content").hide();
-//   $(".accordion").click(function() {
-//     $(".content").show("slow", function() {
-//     });
-//   });
-// });
 $(document).ready(function($) {
    $('#center').find('.accordion','.content').click(function(){
-
      //Expand or collapse this panel
      $(this).next().slideToggle('slow');
-
      //Hide the other panels
      $(".content").not($(this).next()).slideUp('slow');
-
    });
  });
